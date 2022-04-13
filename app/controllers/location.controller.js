d2ecf8ae5d7ed886900aadf3acc114b0d3fd7ba6
mmihaylov-aux <71435826+mmihaylov-aux@users.noreply.github.com>
2022-04-13 17:54:12 +0300

    Removed unused import

--- app/controllers/location.controller.js
+++ app/controllers/location.controller.js
@@ -1,5 +1,4 @@
 const Location = require('../models/location.model')
-const mongoose = require('mongoose')
 
 exports.getLocations = async (req, res) => {
     let locations = await Location.find().exec()
